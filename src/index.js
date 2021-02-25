7cff5e25325c72a9e8fd87fef964684cccddde23
Levchenko41 <lev4encko.vasia@gmail.com>
2021-02-25 23:23:01 +0300

    feat: implemented a function that swaps the digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let k = Math.abs(n).toString().split("").reverse().join("");
+    k = +k;
+    if (k % 10 == 0) {
+        return Math.abs(k / 10)
+    }
+    return Math.abs(k);
 }
